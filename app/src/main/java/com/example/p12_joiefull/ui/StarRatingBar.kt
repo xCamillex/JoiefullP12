6e1f49bdc600c3858c7c77e77377d91c7ffd2333
xcami <xcamille2605x@gmail.com>
2025-03-27 15:10:52 +0100

    Création de "StarRatingBar"

---
+++ app/src/main/java/com/example/p12_joiefull/ui/StarRatingBar.kt
@@ -0,0 +1,69 @@
+package com.example.p12_joiefull.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.p12_joiefull.R
+
+
+@Composable
+fun StarRatingBar(
+    maxStars: Int = 5,
+    rating: Float,
+    onRatingChanged: (Float) -> Unit
+) {
+    val density = LocalDensity.current.density
+    val starSize = (12f * density).dp
+    val starSpacing = (0.5f * density).dp
+
+    Row(
+        modifier = Modifier.selectableGroup(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        for (i in 1..maxStars) {
+            val isSelected = i <= rating
+            // Use Filled Star if selected, Outlined Star if not
+            val icon = if (isSelected) painterResource(R.drawable.star) else painterResource(R.drawable.star_outline)
+            val iconTintColor = if (isSelected) Color(0xFFFFC700) else Color.Black // Adjust color as needed
+
+            Icon(
+                painter = icon,
+                contentDescription = "rating $i stars",
+                tint = iconTintColor,
+                modifier = Modifier
+                    .selectable(
+                        selected = isSelected,
+                        onClick = {
+                            onRatingChanged(i.toFloat())
+                        }
+                    )
+                    .width(starSize)
+                    .height(starSize)
+            )
+
+            if (i < maxStars) {
+                Spacer(modifier = Modifier.width(starSpacing))
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun PreviewStarRatingBar() {
+    StarRatingBar(
+        rating = 3f,
+        onRatingChanged = {}
+    )
+}
