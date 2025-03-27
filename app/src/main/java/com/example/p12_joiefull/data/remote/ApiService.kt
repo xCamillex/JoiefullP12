824fe2d8823e3d1d769eee7d357d8004dbca6c43
xcami <xcamille2605x@gmail.com>
2025-03-27 12:46:25 +0100

    Création interface "ApiService"

---
+++ app/src/main/java/com/example/p12_joiefull/data/remote/ApiService.kt
@@ -0,0 +1,31 @@
+package com.example.p12_joiefull.data.remote
+
+import com.example.p12_joiefull.model.Product
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("main/api/clothes.json")
+    suspend fun getProducts(): List<Product>
+}
+
+object RetrofitInstance {
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://raw.githubusercontent.com/OpenClassrooms-Student-Center/D-velopper-une-interface-accessible-en-Jetpack-Compose/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    val api: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
