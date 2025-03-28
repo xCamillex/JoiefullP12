fa7e3608367cb4a4cb7292dca76f43a28b88cf15
xcami <xcamille2605x@gmail.com>
2025-03-28 18:39:49 +0100

    Mise à jour "StarRatingBar" avec modification des items "star" et "star_outline" (arrondie) pour correspondre au figma

--- app/src/main/java/com/example/p12_joiefull/ui/StarRatingBar.kt
+++ app/src/main/java/com/example/p12_joiefull/ui/StarRatingBar.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.p12_joiefull.R
+import kotlin.math.roundToInt
 
 
 @Composable
@@ -26,7 +27,7 @@ fun StarRatingBar(
 ) {
     val density = LocalDensity.current.density
     val starSize = (12f * density).dp
-    val starSpacing = (0.5f * density).dp
+    val starSpacing = (7f * density).dp
 
     Row(
         modifier = Modifier.selectableGroup(),
@@ -35,8 +36,13 @@ fun StarRatingBar(
         for (i in 1..maxStars) {
             val isSelected = i <= rating
             // Use Filled Star if selected, Outlined Star if not
-            val icon = if (isSelected) painterResource(R.drawable.star) else painterResource(R.drawable.star_outline)
-            val iconTintColor = if (isSelected) Color(0xFFFFC700) else Color.Black // Adjust color as needed
+            val icon = if (isSelected) {
+                painterResource(R.drawable.star)
+            } else {
+                painterResource(R.drawable.star_outline)
+            }
+            val iconTintColor =
+                if (isSelected) Color(0xFFFFC700) else Color.Black // Adjust color as needed
 
             Icon(
                 painter = icon,
@@ -59,11 +65,12 @@ fun StarRatingBar(
         }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewStarRatingBar() {
     StarRatingBar(
-        rating = 3f,
+        rating = 0f,
         onRatingChanged = {}
     )
 }
